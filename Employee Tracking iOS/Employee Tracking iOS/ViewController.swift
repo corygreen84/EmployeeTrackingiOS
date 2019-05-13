c38979135a55e7703cdfb9dcc41c6af786999220
Cory Green <corygreen84@hotmail.com>
2019-05-13 14:34:42 -0700

    initial commit

---
+++ Employee Tracking iOS/Employee Tracking iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Employee Tracking iOS
+//
+//  Created by Cory Green on 5/13/19.
+//  Copyright © 2019 Cory Green. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
